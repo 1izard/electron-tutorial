582119e514daf8bb972fe8bac1f1229c32f5063b
1izard <guitar.kubittake@gmail.com>
2019-10-06 22:16:59 +0900

    Complete tutorial

---
+++ ts/components/UserForm.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect, MapStateToPropsParam } from 'react-redux';
+import IUser from '../states/IUser';
+import { createChangeUserNameAction } from '../actions/UserNameEvents';
+import store, { IState } from '../Store';
+import { TextBox } from './TextBox';
+
+class UserForm extends React.Component<IUser, {}> {
+    public render() {
+        return (
+            <div>
+                <p>
+                    <TextBox label="Username" type="text" value={this.props.name}
+                        onChangeText={this.onChangeText} />
+                </p>
+                <p>Name: {this.props.name}</p>
+            </div>
+        );
+    }
+
+    private onChangeText = (value: string) => {
+        store.dispatch(createChangeUserNameAction(value));
+    }
+}
+
+const mapStateToProps = (state: IState) => {
+    return state.User;
+}
+
+export default connect(mapStateToProps)(UserForm);
